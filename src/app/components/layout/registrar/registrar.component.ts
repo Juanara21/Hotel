a4a6aeaf46634071fc62835473e2c17b1dba6fa5
Juanara21 <110938196+Juanara21@users.noreply.github.com>
2023-04-08 18:51:22 -0500

    Registrar

--- src/app/components/layout/registrar/registrar.component.ts
+++ src/app/components/layout/registrar/registrar.component.ts
@@ -5,24 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './registrar.component.html',
   styleUrls: ['./registrar.component.css']
 })
-export class RegistrarComponent implements OnInit{
+export class RegistrarComponent {
  
       Sexo: string[]=[];
       value: string[]=[];
-      value1: string[]=[];
-
-      selectedCategory: any = null;
-
-    categories: any[] = [
-        { name: 'Masculino', key: 'M' },
-        { name: 'Femenino', key: 'F' },
-        { name: 'Otro', key: 'O' },
-        { name: 'Indefinido', key: 'I' }
-    ];
-
-    ngOnInit() {
-        this.selectedCategory = this.categories[1];
-    }
+      Nombre1: string[]=[];
+      Nombre2: string[]=[];
+      Apellido1: string[]=[];
+      Apellido2: string[]=[];
+      Correo: string[]=[];
+      Identificacion: string[]=[];
+      
 }
   
 
