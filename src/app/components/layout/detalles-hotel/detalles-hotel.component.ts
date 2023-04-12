58a44955da04bdf0785ea85ad24af4ce5714d76f
joaquinloud <joaquinalberto2809@gmail.com>
2023-04-11 21:42:37 -0500

    k

--- src/app/components/layout/detalles-hotel/detalles-hotel.component.ts
+++ src/app/components/layout/detalles-hotel/detalles-hotel.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-detalles-hotel',
   templateUrl: './detalles-hotel.component.html',
   styleUrls: ['./detalles-hotel.component.css']
 })
-export class ButtonBasicDemo {
+export class DetallesHotelComponent {
   
 }
+
 
