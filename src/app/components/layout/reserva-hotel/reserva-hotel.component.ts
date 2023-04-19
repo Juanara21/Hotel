faaba80f07e77907e3186b17e3d1d16d1fa7e790
Miguel0819R <127533226+Miguel0819R@users.noreply.github.com>
2023-04-19 08:00:44 -0500

    Miguel

--- src/app/components/layout/reserva-hotel/reserva-hotel.component.ts
+++ src/app/components/layout/reserva-hotel/reserva-hotel.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reserva-hotel',
   templateUrl: './reserva-hotel.component.html',
   styleUrls: ['./reserva-hotel.component.css']
 })
-export class ReservaHotelComponent {
-
+export class ReservaHotelComponent implements OnInit{
+  ngOnInit(): void {
+    let name = 'Miguel'
+    console.log(`hola ${name}`)}
 }
